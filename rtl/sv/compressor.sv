6b4e1bd99899accdc54e9cd49d032a7ddb16d974
Xinchao Zha <xinchao.zha@duke.edu>
2023-11-24 22:38:25 -0500

    compressor template

--- rtl/sv/compressor.sv
+++ rtl/sv/compressor.sv
@@ -3,9 +3,9 @@ module compressor(
     input rst_n,
     input [15:0][7:0] outmap_data,     // will start from the first valid data
     input start,
-    input [3:0] outmap_data_valid_num, // from 0-8
+    input [4:0] outmap_data_valid_num, // from 0-16
     input mem_ack, // will not compress new data if memack is not ready
-    output [3:0] valid_taken_num, // the element which has been taken by compressor
+    output [4:0] valid_taken_num, // the element which has been taken by compressor
     output [64:0] compressed_data,
     output mem_req // mem_req will only be assert when compressed data is ready
 );
---
+++ rtl/sv/weight_map.sv
@@ -0,0 +1,8 @@
+module weight_buffer(
+    input clk,
+    input rst_n,
+    in
+);
+
+
+endmodule
