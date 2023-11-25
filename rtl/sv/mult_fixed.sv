37e0d94e43671b8c873c599d4e296829b1744bfd
Jianping Shen <jpshen@umich.edu>
2023-11-24 22:53:56 -0500

    PE Pass mode1 base test

--- rtl/sv/mult_fixed.sv
+++ rtl/sv/mult_fixed.sv
@@ -5,9 +5,10 @@ module mult_fixed(
     input signed [`WDATA_SIZE-1:0] inB,  // signed fixed point (8,6)
     output signed [`MULT_OUT_SIZE-1:0] out // signed fixed point (8,5)
 );
-
+logic signed [`IFDATA_SIZE:0] inA_signed;
+assign inA_signed = {1'b0, inA};
 logic signed [15:0] mult_temp; // (16,13)
-assign mult_temp = inA * inB;
+assign mult_temp = inA_signed * inB;
 assign out = mult_temp[15:8]; // Trim to (8,5)
 // Ideally, overflow should not happen in multiplier
 endmodule
