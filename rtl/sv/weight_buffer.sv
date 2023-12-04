0a8fb00522fef82606028ed83bf35b2f72aa3ff7
xueanchen <xueanche@umich.edu>
2023-12-03 20:42:51 -0500

    weight buffer bug fixed

--- rtl/sv/weight_buffer.sv
+++ rtl/sv/weight_buffer.sv
@@ -1,7 +1,7 @@
 module weight_buffer(
     input            clk,
     input            rst_n,
-    input OP_MODE mode_in,// mode selection
+    input OP_MODE cur_mode,// mode selection
     input start_load,           
     input mem_data_valid,
     input [63:0] weight_data,//8bytes one cycle frome memory
@@ -10,25 +10,20 @@ module weight_buffer(
     //from controller
     input free_weight_buffer,
     //to memory
-    output logic mem_req_delay,//need data or not
+    output logic mem_req,//need data or not
     // to controller
-    output logic ready_to_output_delay,//buffer is full ready to output
+    output logic ready_to_output,//buffer is full ready to output
     //to pe
 
     output logic finish_output_delay,
-    output PE_IN_PACKET packet_out_delay[0:5]//line6
+    output PE_IN_PACKET packet_out_delay[5:0]//line6
 
 );
-    logic mem_req,ready_to_output;
-    PE_IN_PACKET packet_out[0:5];
+    // logic mem_req,ready_to_output;
+    PE_IN_PACKET packet_out[5:0];
     // localparam buffer_maxrow_idx=`L1_FILTER_SIZE*`MULT_OUT_SIZE;//44*11*8bits
     logic [5:0] row_idx;        
     logic  cycle_count;    
-    OP_MODE cur_mode;
-    always_ff @(posedge clk) begin
-        if(rst_n) cur_mode <= MODE1;
-        else  cur_mode <= mode_in;
-    end 
 //input counter calculation,
     always_ff@(posedge clk) begin
         if(rst_n||free_weight_buffer)begin
@@ -37,7 +32,7 @@ module weight_buffer(
         end
         else if(mem_data_valid&&mem_req&&start_load) begin
             case (cur_mode)
-            MODE1,MODE2: begin // mode1 ? mode2
+            MODE1,MODE2: begin 
                     cycle_count <= cycle_count+1;
                     if (cycle_count == 1) begin
                         row_idx<=row_idx+1;
@@ -70,7 +65,7 @@ module weight_buffer(
                     end
                 end
                 MODE3: begin 
-                    if (row_idx==19&&cycle_count==1) begin
+                    if (row_idx==19) begin
                         mem_req=0;
                         ready_to_output=1;
                     end
@@ -80,7 +75,7 @@ module weight_buffer(
                     end
                 end
                 MODE4: begin 
-                    if (row_idx==11&&cycle_count==1) begin
+                    if (row_idx==11) begin
                         mem_req=0;
                         ready_to_output=1;
                     end
@@ -92,10 +87,7 @@ module weight_buffer(
             endcase
         end
     end
-      always_ff@(posedge clk) begin
-                            mem_req_delay<=mem_req;
-                        ready_to_output_delay<=ready_to_output;
-                        end
+
 
 ///buffer register file 
     localparam buffer_row_length=`WDATA_SIZE*`L1_FILTER_SIZE;
@@ -105,7 +97,7 @@ module weight_buffer(
         if(rst_n||free_weight_buffer)begin
             filter_ram<='0;
         end
-        else if(mem_data_valid&&mem_req_delay&&start_load) begin
+        else if(mem_data_valid&&mem_req&&start_load) begin
         case (cur_mode)
             MODE1,MODE2: begin 
              if (cycle_count == 0) begin
@@ -123,20 +115,21 @@ module weight_buffer(
     end
 // ////////////////////////output logic/////////////////////////////////////////////////////
 //output counter calculation,
-logic output_finish_comb,output_finish;
+logic       output_finish_comb;
+logic       output_finish;
 logic [1:0] output_cycle_counter;
 logic [1:0] output_layer_idx;
-localparam [5:0] offset [3:0]={33,22,11,0};
-localparam [5:0] offset3 [3:0]={15,10,5,0};
-localparam [5:0] offset4 [3:0]={9,6,3,0};
-localparam [5:0] idx4 [5:0]={2,1,0,2,1,0};
+logic [5:0] offset [3:0]={33,22,11,0};
+logic [3:0] offset3 [3:0]={15,10,5,0};
+logic [3:0] offset4 [3:0]={9,6,3,0};
+logic [2:0] idx4 [5:0]={2,1,0,2,1,0};
     always_ff@(posedge clk) begin
         if(rst_n||!ready_to_output||!output_filter)begin
             output_layer_idx<=0;
             output_cycle_counter<=0;
         end
 
-        else if(!output_finsih_comb)begin
+        else if(!output_finish_comb)begin
 
            if(output_cycle_counter==2) begin
             output_cycle_counter<=0;
@@ -148,20 +141,18 @@ localparam [5:0] idx4 [5:0]={2,1,0,2,1,0};
 assign output_finish_comb=(output_layer_idx<3)? 0:(output_cycle_counter==2)? 1:0;
     always_ff@(posedge clk) begin
 
-            output_finsih<=output_finsih_comb;
+            output_finish<=output_finish_comb;
 
         end
 
  always_ff@(posedge clk) begin
-        if(rst_n||(!ready_to_output)||(!output_filter)||(output_finsih))begin
+        if(rst_n||(!ready_to_output)||(!output_filter)||(output_finish))begin
             for (int i = 0; i < 6; i++) begin
                 packet_out[i]<='{default:0};
             end
         end
 
-        else if(!output_finsih_comb)  begin
-
-
+        else if(!output_finish_comb)  begin
         case (cur_mode)
             MODE1: begin 
                 if(output_cycle_counter==0)begin
@@ -243,21 +234,21 @@ assign output_finish_comb=(output_layer_idx<3)? 0:(output_cycle_counter==2)? 1:0
                     for (logic[2:0] i = 0; i < 6; i++) begin
                         packet_out[i].data<={8'b0, filter_ram[idx4[i]+offset4[output_layer_idx]][87:64]};
                         packet_out[i].valid<=1;
-                        packet_out[i].packet_idx<={output_layer_idx,i};
+                        packet_out[i].packet_idx<={output_layer_idx,idx4[i]};
                     end
                 end
                 if(output_cycle_counter==1)begin
                     for (logic[2:0] i = 0; i < 6; i++) begin
                         packet_out[i].data<=filter_ram[idx4[i]+offset4[output_layer_idx]][63:32];
                         packet_out[i].valid<=1;
-                        packet_out[i].packet_idx<={output_layer_idx,i};
+                        packet_out[i].packet_idx<={output_layer_idx,idx4[i]};
                     end
                 end
                 if(output_cycle_counter==2)begin
                     for (logic[2:0] i = 0; i < 6; i++) begin
                         packet_out[i].data<=filter_ram[idx4[i]+offset4[output_layer_idx]][31:0];
                         packet_out[i].valid<=1;
-                        packet_out[i].packet_idx<={output_layer_idx,i};
+                        packet_out[i].packet_idx<={output_layer_idx,idx4[i]};
                     end
                 end             
             end
@@ -265,7 +256,7 @@ assign output_finish_comb=(output_layer_idx<3)? 0:(output_cycle_counter==2)? 1:0
         end
     end
     always_ff@(posedge clk) begin
-    finish_output_delay<=output_finsih;
+    finish_output_delay<=output_finish;
     packet_out_delay[0]<=packet_out[0];
     packet_out_delay[1]<=packet_out[1];
     packet_out_delay[2]<=packet_out[2];
